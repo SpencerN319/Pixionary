944b9ea0daac36aa10fc26c9ea36392db5236fc0
Levirclark <levirclark@gmail.com>
2018-01-28 22:00:17 -0600

    Added a new login activity, still need to hook the activity up

---
+++ PictureGuesser/app/src/main/java/sb_3/pictureguesser/LoginActivity.java
@@ -0,0 +1,15 @@
+package sb_3.pictureguesser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+    }
+}
