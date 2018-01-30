7ce07f81ff1cd61142107caccb7030b1a8df19b0
reinx058 <reinx058@iastate.edu>
2018-01-30 17:41:20 -0600

    Add PlayActivity to the app.

--- PictureGuesser/app/src/main/java/sb_3/pictureguesser/PlayActivity.java
+++ PictureGuesser/app/src/main/java/sb_3/pictureguesser/PlayActivity.java
@@ -1,10 +1,14 @@
 package sb_3.pictureguesser;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -28,13 +32,38 @@ public class PlayActivity extends AppCompatActivity {
         picGuess = (ImageView) findViewById(R.id.imgGame);
 
         presentImage(picGuess);
+
+        btnGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendGuess(etGuess.getText().toString());
+            }
+        });
     }
 
     protected void sendGuess(String guess) {
         Log.i("PlayActivity", guess);
+
+        if (etGuess.getText().toString().equals("cat")) {
+            AlertDialog.Builder endBuilder = new AlertDialog.Builder(PlayActivity.this);
+            endBuilder.setTitle("Game Over!");
+            endBuilder.setMessage("You Won!");
+            endBuilder.setPositiveButton("Exit to Main Menu", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent i = new Intent(PlayActivity.this, MainMenuActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            });
+            endBuilder.create();
+            endBuilder.show();
+        }
     }
 
     protected void presentImage(ImageView pic) {
+
+        //testing
         String imageName = "cat.jpg";
 
         try {
