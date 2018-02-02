f338ac6929e5e5153222da2f7368748f4c3ead2d
KoleSwesey <kwswesey@iastate.edu>
2018-02-02 15:25:56 -0600

    added more features

--- imgbreak.java
+++ imgbreak.java
@@ -1,23 +1,36 @@
 import java.awt.image.BufferedImage;
-//breaks the image into PIXELS
-public class imgbreaker{
+import java.util.Random;
+//breaks the image into PIXELS and also transfers them
+public class imgbreak{
     
    BufferedImage img;
    int[][] pixels;
+    boolean[][] used;
+    int totalpixels;
+    int sent;
+    int height;
+    int width; 
+    Random rand = new Random();
+    boolean guessed = false;
+  
     
-    public imgbreaker (BufferedImage i)
+    public imgbreak (BufferedImage i)
     {
 	img = i; 
     }
 
     public void breakImage()
     {   //get some dimensions
-	int height = img.getHeight();
-	int width = img.getWidth();
-	//maybe not declare this here
+	 height = img.getHeight();
+	 width = img.getWidth();
+	 
+	
 	pixels = new int[height][width];
-
+	used = new boolean[height][width];
+	totalpixels = width * height;
+	
 	for (int y = 0; y < height; y++)
+	    
 	    {
 		for (int x = 0; x < width; x++)
 		    {
@@ -27,8 +40,33 @@ public class imgbreaker{
 	    }
 	
     }
-
-
     
-    
+    public void sendPixels()
+    {   
+	while (sent < totalpixels && guessed == false)
+	    {
+		//declared to make compiler happy
+		int pixx = 0;
+		int pixy = 0;
+		boolean found = false;
+		while (found == false)
+		    {
+			pixx = rand.nextInt(width);
+			pixy = rand.nextInt(height);
+			if (used[pixy][pixx] == false)
+			    found = true;
+		    }
+		used [pixy][pixx] = true;
+		/* pseudo code
+		   socket.sendInts (pixx, pixy, pixels[pixx][pixy]);
+		*/
+		sent++;
+	    }
+		
+    }
+    public void setGuessed()
+    {
+	guessed = true;
+    }
+        
 }
