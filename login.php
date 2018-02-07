10c9dba271edde22f55d3160ba94140cb804c333
kwswesey <kwswesey@iastate.edu>
2018-02-07 18:15:30 +0000

    Update login.php, I can sanitize inputs later; what I did broke it.

--- login.php
+++ login.php
@@ -10,8 +10,8 @@ if (!$con)
 	  die('Could not connect to mysql server ' . mysqli_error());
   }
 
-$username =mysql_real_escape_string($_GET['username']);  
-$password = mysql_real_escape_string($_GET['password']);
+$username =$_GET['username'];  
+$password =$_GET['password'];
 
 $result = (mysqli_query($con, "SELECT * FROM logins WHERE username = '$username' AND password = '$password'"));
 $count =mysqli_num_rows($result);
