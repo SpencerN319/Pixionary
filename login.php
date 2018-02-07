70c578c4fc7959c6a9f49b2f24b354c9a3ee708d
KoleSwesey <kwswesey@iastate.edu>
2018-02-07 11:51:00 -0600

    inputs correctly sanitized now

--- login.php
+++ login.php
@@ -10,10 +10,10 @@ if (!$con)
 	  die('Could not connect to mysql server ' . mysqli_error());
   }
 
-$username = $_GET['username'];  
-$password = $_GET['password'];
+$username =mysql_real_escape_string($_GET['username']);  
+$password = mysql_real_escape_string($_GET['password']);
 
-$result = mysql_real_escape(mysqli_query($con, "SELECT * FROM logins WHERE username = '$username' AND password = '$password'"));
+$result = (mysqli_query($con, "SELECT * FROM logins WHERE username = '$username' AND password = '$password'"));
 $count =mysqli_num_rows($result);
 
 if(!$count)
