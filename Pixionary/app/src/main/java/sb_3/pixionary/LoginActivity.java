6e04220aefc0242efc9cc20490cb2eec0290ec17
Spencer Nicol <stnicol@iastate.edu>
2018-02-07 20:09:08 -0600

    Added semi functionality to loginActivity

--- Pixionary/app/src/main/java/sb_3/pixionary/LoginActivity.java
+++ Pixionary/app/src/main/java/sb_3/pixionary/LoginActivity.java
@@ -8,24 +8,67 @@ import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText usernameTextbox, passwordTextbox;
+    private String username, password;
+    private String[] upPair = new String[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+    }
+
+    public void init() {
+        usernameTextbox = (EditText) findViewById(R.id.editText_username);
+        passwordTextbox = (EditText) findViewById(R.id.editText_password);
+
+        username = String.valueOf(usernameTextbox.getText());
+        password = String.valueOf(passwordTextbox.getText());
     }
 
     public void returnLoginInfo(View view) {
-        EditText usernameTextbox = (EditText) findViewById(R.id.editText_username);
-        EditText passwordTextbox = (EditText) findViewById(R.id.editText_password);
-        String username = String.valueOf(usernameTextbox.getText());
-        String password = String.valueOf(passwordTextbox.getText());
-        String[] upPair = new String[2];
         upPair[0] = username;
         upPair[1] = password;
         Intent retval = new Intent();
-        retval.putExtra("UPPair", upPair);
+        retval.putExtra("upPair",upPair);
         setResult(RESULT_OK, retval);
         finish();
     }
+
+    // To be used in Create Account Activity
+    /*
+
+    private boolean validUsername(String string) {
+        //validate username;
+        if(string.equals("")){
+            usernameTextbox.setError("Enter a username");
+            return false;
+        } else if(string.length() > 20){
+            usernameTextbox.setError("Max 20 characters");
+            return false;
+        } else if(string.length() < 5){
+            usernameTextbox.setError("Minimum 5 characters");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validPassword(String string){
+        if(string.equals("")){
+            passwordTextbox.setError("Enter a username");
+            return false;
+        } else if(string.length() > 8){
+            passwordTextbox.setError("Max 8 Characters");
+            return false;
+        } else if(string.length() < 6){
+            passwordTextbox.setError("Minimum 6 Characters");
+            return false;
+        }
+        return true;
+    }
+
+    */
+
+
 }
