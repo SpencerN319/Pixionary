ac4d1a22d3b27fd0f564e4b6fec1aa2731f753ca
KoleSwesey <kwswesey@iastate.edu>
2018-02-12 12:31:53 -0600

    imgtaker works now

--- Server/imgtaker.bash
+++ Server/imgtaker.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
-wget -O url.txt "https://api.duckduckgo.com/?q=moon&format=json&pretty=1";
-cat source.html | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*.jpg" | head -n 1;
+#uses duckduckgo api to download the first image result for the word given as an arg
 
-#right now this gets an isolated image url
+part1='https://api.duckduckgo.com/?q=';
+part2='&format=json&pretty=1';
+website=$part1$1$part2;
+
+echo $website;
+wget -O url.txt $website;
+
+cat url.txt | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*.jpg" | head -n 1 > link.txt;
+
+wget -i link.txt -O image.jpg;
+
+rm link.txt;
+rm url.txt;
+
