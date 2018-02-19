4f137505714968e704d8880598ee2f77babcdac8
reinx058 <reinx058@iastate.edu>
2018-02-19 11:32:09 -0600

    Working on profile info activity

---
+++ Pixionary/app/src/main/java/sb_3/pixionary/ProfileActivity.java
@@ -0,0 +1,13 @@
+package sb_3.pixionary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
