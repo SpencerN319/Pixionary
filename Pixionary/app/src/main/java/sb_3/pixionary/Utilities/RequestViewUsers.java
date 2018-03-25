395229bb55f2998287ffd2f76099acc1cc6072ae
Spencer Nicol <stnicol@iastate.edu>
2018-03-25 17:47:29 -0500

    request user needs implementation

--- Pixionary/app/src/main/java/sb_3/pixionary/Utilities/RequestViewUsers.java
+++ Pixionary/app/src/main/java/sb_3/pixionary/Utilities/RequestViewUsers.java
@@ -1,8 +1,31 @@
 package sb_3.pixionary.Utilities;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by spencern319 on 3/25/18.
  */
 
-public class RequestViewUsers {
+public class RequestViewUsers extends StringRequest{
+    private static final String CATEGORIES_URL = "http://proj-309-sb-3.cs.iastate.edu:80/"; //TODO URL needs to be changed.
+    private Map<String, String> parameters;
+
+    public RequestViewUsers(String username, int pageRequested, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        super(Request.Method.POST, CATEGORIES_URL, listener, errorListener);
+        String page = String.valueOf(pageRequested);
+        parameters = new HashMap<>();
+        parameters.put("username", username);
+        parameters.put("page", page);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return parameters;
+    }
 }
