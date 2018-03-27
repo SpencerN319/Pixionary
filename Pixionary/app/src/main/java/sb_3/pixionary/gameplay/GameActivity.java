f6c831dbe7f7ed77cf77413c7e6c202275e3a274
reinx058 <reinx058@iastate.edu>
2018-03-26 23:54:13 -0500

    Changed to receive all pixels at once, UI wasn't updating

--- Pixionary/app/src/main/java/sb_3/pixionary/gameplay/GameActivity.java
+++ Pixionary/app/src/main/java/sb_3/pixionary/gameplay/GameActivity.java
@@ -3,16 +3,21 @@ package sb_3.pixionary.gameplay;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 import SaveData.UserDataDBHandler;
@@ -22,78 +27,73 @@ import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import sb_3.pixionary.Adapters.GuessListAdapter;
+import sb_3.pixionary.ImageBuilder.Pixel;
 import sb_3.pixionary.R;
 import sb_3.pixionary.Utilities.POJO.ShortUser;
 import sb_3.pixionary.Utilities.POJO.User;
 import sb_3.pixionary.Utilities.RealTimeUpdater;
 
+import static sb_3.pixionary.MainMenuActivity.user;
+
 //TODO still need to add a view to see the players in the lobby. --- Not important RN.
 public class GameActivity extends AppCompatActivity {
 
     private static final String TAG = RealTimeUpdater.class.getSimpleName();
-    private String url = "ws://proj-309-sb-3.cs.iastate.edu:8080/name"; //TODO this will need to be changed.
+    private String url = "ws://proj-309-sb-3.cs.iastate.edu:8080/name";
+    private Context context;
     private OkHttpClient okHttpClient;
     private WebSocket webSocket;
-    private Context context;
 
-    //Stuff for lobby
     private int gameID = -1;
-    private int gameType;
+    private String playlistName;
     private User user;
-    private String playlist;
-    private ArrayList<ShortUser> players;
-    private ArrayList<String> chat;
-    private TextView lobbyName;
-    private TextView gameName;
-    private TextView playerUpdate;
-    private ImageView previewImage;
-
-    //Stuff for Play
-    private Bitmap bitmap;
     private ArrayList<String> listOfOptions;
-    private boolean listChanged = false;
-    private int numImages;
+    private ArrayAdapter<String> adapter;
+    private Bitmap bitmap;
+
     private Button sendGuess;
     private ImageView image;
     private ListView guessList;
 
-    //Stuff for lobby
-    private Button chatButton;
-    private Button startButton;
-    private Button leaveButton;
-
+    //Image stuff
+    int width;
+    int height;
+    int x = 0;
+    int y = 0;
+    int[] pixels;
+    boolean[] pixelUsed;
+    Handler handler = null;
+    Runnable runnable = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = this;
-        UserDataDBHandler db = new UserDataDBHandler(this);
-        User user = db.getUser("0");
-        String playlistName = getIntent().getStringExtra("playlist");
-        listOfOptions = new ArrayList<>();
-        guessList = (ListView) findViewById(R.id.list_of_guesses);
         setContentView(R.layout.activity_play);
-        startButton = (Button) findViewById(R.id.start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.i("Sending Start", "");
-                    sendStart();
-                }
-            });
+        context = this;
 
+        guessList = (ListView) findViewById(R.id.list_of_guesses);
         sendGuess = (Button) findViewById(R.id.btnSendGuess);
+        image = (ImageView) findViewById(R.id.imgGame);
+
+        connect();
+
+        UserDataDBHandler db = new UserDataDBHandler(this);
+        user = db.getUser("0");
+        playlistName = getIntent().getStringExtra("playlist");
+        listOfOptions = new ArrayList<>();
+        listOfOptions.add("Fake 1");
+        listOfOptions.add("Fake 2");
+//        GuessListAdapter adapter = new GuessListAdapter(context, listOfOptions);
+        adapter = new ArrayAdapter<>(context, R.layout.guess_list, R.id.guess1, listOfOptions);
+        guessList.setAdapter(adapter);
+
         sendGuess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //TODO Create a method to send the Guess
             }
         });
 
-        //For lobby
-        chatButton = (Button) findViewById(R.id.open_chat);
-        leaveButton = (Button) findViewById(R.id.leave_button);
-        image = (ImageView) findViewById(R.id.imgGame);
     }
 
     public void connect() {
@@ -104,11 +104,9 @@ public class GameActivity extends AppCompatActivity {
             public void onOpen(WebSocket webSocket, Response response) {
 
                 if (user.getUserType().equals("host")) {
-                    String message = "create," + user.getUsername() + "," + playlist;
-                    webSocket.send(message);
+                    createGame();
                 } else {
-                    String message = "join" + gameID;
-                    webSocket.send(message);
+                    joinGame();
                 }
                 Log.i("Response:", response.message());
             }
@@ -116,7 +114,7 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onMessage(WebSocket webSocket, String text) {
                 final String message = text;
-                ((Activity)context).runOnUiThread(new Runnable() {
+                runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         messageReceived(message);
@@ -138,6 +136,21 @@ public class GameActivity extends AppCompatActivity {
         webSocket = okHttpClient.newWebSocket(request, webSocketListener);
     }
 
+    private void createGame() {
+        String message = "create," + user.getUsername() + "," + playlistName;
+        webSocket.send(message);
+    }
+
+    private void joinGame() {
+        String message = "join" + gameID;
+        webSocket.send(message);
+    }
+
+    private void sendStart() {
+        String message = "start," + gameID;
+        webSocket.send(message);
+    }
+
     private void messageReceived(String message) {
         Scanner scanner = new Scanner(message);
         String type = scanner.next();
@@ -147,7 +160,6 @@ public class GameActivity extends AppCompatActivity {
                 createGameReaction(message);
                 break;
             case "START":
-                ((Activity)context).setContentView(R.layout.activity_play);
                 break;
             case "WORD":
                 addWord(message);
@@ -159,9 +171,10 @@ public class GameActivity extends AppCompatActivity {
                 setHeightAndWidth(message);
                 break;
             case "px":
-                addPixel(message);
+                //addPixel(message);
+                receiveAllPixels(message);
                 break;
-            case "ROUNDEND": //FIXME Create something for wiping the Bitmap.
+            case "ROUNDEND":
                 wipeBitmap();
                 break;
             default:
@@ -170,17 +183,11 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void setWords() {
-        GuessListAdapter guessListAdapter = new GuessListAdapter(context, listOfOptions);
-        guessList.setAdapter(guessListAdapter);
+        //TODO not sure what to do here.
     }
 
     private void wipeBitmap() {
         bitmap = Bitmap.createBitmap(0, 0, Bitmap.Config.ARGB_8888);
-    }
-
-    private void sendStart() {
-        String message = "start," + gameID;
-        webSocket.send(message);
     }
 
     private void addPixel(String message) {
@@ -195,11 +202,60 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
+    private void receiveAllPixels(String message) {
+        Scanner scanner = new Scanner(message);
+        String command = scanner.next();
+        if (command.equals("px")) {
+            for (int i = 0; i < pixels.length; i++) {
+                x++;
+                if (x >= width) {
+                    y++;
+                    x = 0;
+                }
+                pixels[y*width + x] = scanner.nextInt();
+            }
+        }
+        //FIXME Might need a separate method to check for when the runnable should start then we can put the method in the onCreate()
+        pixelUsed = new boolean[pixels.length];
+        Arrays.fill(pixelUsed, false);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Pixel pix = getUnusedPixel();
+                postPixel(bitmap, pix);
+                handler.postDelayed(runnable, 3);
+            }
+        };
+        handler.postDelayed(runnable, 3);
+    }
+
+    private Pixel getUnusedPixel() {
+        Random random = new Random();
+        boolean used;
+        Pixel pix;
+        do {
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            used = pixelUsed[x*y];
+            pix = new Pixel(x, y, pixels[x*y]);
+        } while (used);
+
+        return pix;
+    }
+
+    private void postPixel(Bitmap bitmap, Pixel pixel) {
+        int x = pixel.getXPosition();
+        int y = pixel.getYPosition();
+        bitmap.setPixel(x, y, pixels[y*width + x]);
+    }
+
     private void createGameReaction(String message) {
         Scanner scanner = new Scanner(message);
         if(scanner.next().equals("Creating")) {
             gameID = scanner.nextInt();
             Log.i("GameID", String.valueOf(gameID));
+            sendStart();
         }
     }
 
@@ -210,14 +266,12 @@ public class GameActivity extends AppCompatActivity {
             String word = scanner.next();
             Log.i("Word Read", word);
             listOfOptions.add(word);
-            listChanged = true;
+            adapter.notifyDataSetChanged();
         }
     }
 
     private void setHeightAndWidth(String message) {
         Scanner scanner = new Scanner(message);
-        int height = 0;
-        int width = 0;
         String command = scanner.next();
         if (command.equals("HEIGHT") && scanner.hasNextInt()) {
             height = scanner.nextInt();
@@ -228,7 +282,33 @@ public class GameActivity extends AppCompatActivity {
             width = scanner.nextInt();
             Log.i("Width:", String.valueOf(width));
         }
+        pixels = new int[width*height];
         bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        image.setImageBitmap(bitmap);
+    }
+
+    class UpdateImage extends AsyncTask<Void, Void, Void> {
+
+        String message;
+
+        public UpdateImage(String message) {
+            this.message = message;
+        }
+
+        @Override
+        protected Void doInBackground(Void...voids) {
+            Scanner scanner = new Scanner(message);
+            String command = scanner.next();
+            if (command.equals("px")) {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                int color = scanner.nextInt();
+                bitmap.setPixel(x, y, color);
+                Log.i("Pixel Set", String.valueOf(color));
+            }
+            return null;
+        }
+
     }
 
 }
--- Pixionary/app/src/main/java/sb_3/pixionary/gameplay/LobbyActivity.java
+++ Pixionary/app/src/main/java/sb_3/pixionary/gameplay/LobbyActivity.java
@@ -2,10 +2,31 @@ package sb_3.pixionary.gameplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import sb_3.pixionary.R;
+import sb_3.pixionary.Utilities.POJO.ShortUser;
+import sb_3.pixionary.Utilities.POJO.User;
 
 public class LobbyActivity extends AppCompatActivity {
+
+
+    private int gameType;
+    private User user;
+    private String playlist;
+    private ArrayList<ShortUser> players;
+    private ArrayList<String> chat;
+    private TextView lobbyName;
+    private TextView gameName;
+    private TextView playerUpdate;
+    private ImageView previewImage;
+    private Button chatButton;
+    private Button startButton;
+    private Button leaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
