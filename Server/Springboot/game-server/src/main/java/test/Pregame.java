48e02769bb0144cdcc9c4c23fed5c27f3e1ec57c
kwswesey <kwswesey@iastate.edu>
2018-04-02 15:06:53 +0000

    Delete Pregame.java, ended up not being needed

--- Server/Springboot/game-server/src/main/java/test/Pregame.java
+++
@@ -1,9 +0,0 @@
-//game class only seems to support running games. This would be used for lobbies with players waiting for game to start yaknow
-//maybe we don't need this as a separate class
-package test;
-
-import java.util.ArrayList;
-
-public class Pregame {
-
-}
