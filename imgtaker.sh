fae0ac0f35a0a295d3fc2dd21a3adfa98ebde4c3
kwswesey <kwswesey@iastate.edu>
2018-04-22 22:50:22 +0000

    minor bug fix + altered plan

--- imgtaker.sh
+++ imgtaker.sh
@@ -8,13 +8,15 @@ website=$part1$1;
 echo $website;
 wget -O url.txt $website;
 
-cat url.txt | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*.jpg" | head -n 10 > link.txt;
+cat url.txt | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*.jpg" | head -n 12 > link.txt;
 
 for value in {1..12}
 do
 	head -n $value link.txt | tail -n 1 > onelink.txt
 	echo $url
-	wget -i onelink.txt  -O $value.jpg;
+	wget -i onelink.txt -O $value.jpg
+	cat $value.jpg | base64 >$value.txt
+
 done
 rm url.txt;
 rm link.txt;
